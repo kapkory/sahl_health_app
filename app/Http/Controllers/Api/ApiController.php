524fe37256d400d8aa8f82a5a8b9960ee23bac2b
levis <leviskapkory@gmail.com>
2020-01-03 22:31:22 +0300

    test message details

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -7,6 +7,7 @@ use App\Models\Core\Identification;
 use App\Models\Core\InstitutionLevel;
 use App\Models\Core\OrganizationType;
 use App\Models\Core\PackageCategory;
+use App\Repositories\TechpitMessageRepository;
 use Carbon\Carbon;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
@@ -34,28 +35,15 @@ class ApiController extends Controller
     }
 
     public function sendSms(){
-//        header("Content-Type: application/json");
-         $username = 'SAHL';
-         $password = '!Kitale2019';
-         $businessCode = 'TPL-SAH-013';
+
          $timestamp = Carbon::now('Africa/Nairobi')->format('YmdHis');
 //         dd($timestamp,date('YmdHis'));
-         $data =  [
-             'token' => base64_encode(hash('sha256', $username . $password . $timestamp)),
-             'timestamp' => $timestamp,
-             'business_code' => $businessCode,
-             'short_code' => 'SAHL-HEALTH',
-             'external_bulk_id' => Str::random(14),
-             'message' => "test Message by Levis",
-             'schedule_time' => Carbon::now('Africa/Nairobi')->format("Y-m-d H:i:s"),
-             'addresses' => ['254712137367']
-         ];
 
-        $client = new Client();
+        $message= 'Hello there, This is a test Message';
+        $address= ['254712137367'];
+        $message = new TechpitMessageRepository();
+        $response = $message->execute($message,$address);
+        return $response;
 
-        $response = $client->post('http://196.13.121.195:9095/external-bulk/create', [
-            'json' =>$data,
-        ]);
-        return json_decode((string) $response->getBody(), true);
     }
 }
--- app/Repositories/TechpitMessageRepository.php
+++ app/Repositories/TechpitMessageRepository.php
@@ -4,13 +4,16 @@
 namespace App\Repositories;
 
 
+use Carbon\Carbon;
 use GuzzleHttp\Client;
+use Illuminate\Support\Str;
 
 class TechpitMessageRepository
 {
     public $username = 'SAHL';
     public $password = '!Kitale2019';
     public $businessCode = 'TPL-SAH-013';
+    public $shortCode = 'SAHL-HEALTH';
     public $token;
     public $externalBulkId;
     public $message;
@@ -19,62 +22,29 @@ class TechpitMessageRepository
     public $client;
     public $baseUrl = 'http://196.13.121.195:9095/';
 
+    public function execute($message, $addresses = []) {
 
-    public function __construct($config = []) {
-        $client = new Client();
-        $this->client = new Client(['baseUrl' => $this->baseUrl]);
-    }
-
-    public function addAddress($address) {
-//        ArrayHelper::setValue($this->addresses,null,$address);
-        $this->addresses[] = $address;
-        return $this->addresses;
-    }
-
-    public function execute($message = null, $addresses = []) {
-        $date = new DateTime();
-
-        return $this->client->post('external-bulk/create', [
-            'token' =>  base64_encode(hash('sha256', $this->username . $this->password . $date->getTimestamp(), true)),
-            'timestamp' => $date->getTimestamp(),
+        $timestamp= Carbon::now('Africa/Nairobi')->format('YmdHis');
+        $data =  [
+            'token' => $this->getToken($timestamp),
+            'timestamp' => $timestamp,
             'business_code' => $this->businessCode,
-            'external_bulk_id' => $this->generateUUID(),
-            'message' => "test",
-            'schedule_time' => date("Y-m-d H:i:s"),
-            'addresses' => ['254712137367']
-        ])->send();
-
-//        return base64_encode(hash('sha256', $this->username . $this->password . $date->getTimestamp(), true));
-
-        $data = [
-            'token' => base64_encode(hash('sha256', $this->username . $this->password . $date->getTimestamp(), true)),
-            'business_code' => $this->businessCode,
-            'external_bulk_id' => rand(),
-            'timestamp' => $date->getTimestamp(),
+            'short_code' => $this->shortCode,
+            'external_bulk_id' => Str::random(14),
             'message' => $message,
-            'schedule_time' => date("Y-m-d H:i:s"),
-            'addresses' => ['254712137367'],
+            'schedule_time' => Carbon::now('Africa/Nairobi')->format("Y-m-d H:i:s"),
+            'addresses' => $addresses
         ];
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->baseUrl . 'external-bulk/create');
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 180);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        $results = curl_exec($ch);
-        curl_close($ch);
-        echo 'here';
+        $client = new Client();
+        $response =  $client->post($this->baseUrl.'/external-bulk/create', [
+            'json' =>$data,
+        ]);
 
-        return $results;
-
+        return json_decode((string) $response->getBody(), true);
 
     }
 
-    public function getToken() {
-        $date = new DateTime();
-        return base64_encode(hash('sha256', $this->username . $this->password . $date->getTimestamp(), true));
+    public function getToken($timestamp) {
+        return base64_encode(hash('sha256', $this->username . $this->password . $timestamp, true));
     }
 }

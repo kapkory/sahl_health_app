92666e5a714f988d1d39f0247f14cf26dc5bd941
levis <leviskapkory@gmail.com>
2020-01-10 07:42:28 +0300

    clear workspace

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -11,19 +11,6 @@ use Illuminate\Support\Str;
 class IndexController extends Controller
 {
     public function index(){
-        $services = Storage::disk('local')->get('system/kmfl_services.json');
-        $services = json_decode($services);
-        foreach ($services->service as $service){
-
-            $serv = new Service();
-            $serv->name = $service->name;
-            $serv->slug = Str::slug($service->name);
-            $serv->user_id = 1;
-            $serv->status = 1;
-            $serv->organization_type_id = 1;
-            $serv->save();
-        }
-        dd();
         return view($this->folder.'index');
     }
 }

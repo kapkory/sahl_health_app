754ffbced0df1af964b8255573986005f9107572
levis <leviskapkory@gmail.com>
2020-01-13 11:47:39 +0300

    recent updates

--- resources/views/core/member/dependants.blade.php
+++ resources/views/core/member/dependants.blade.php
@@ -37,18 +37,7 @@
             <div class="container py-md-5">
                 <div class="rounded">
                     <div class="row py-4">
-                        <div class="col-md-8 pt-md-3 mt-md-3">
-                            <h3 class="text-light">
-                                <b>ACCESS AFFORDABLE HEALTHCARE <br>&nbsp;&nbsp;&nbsp; ANYWHERE, ANYTIME</b>
-                            </h3>
-                            <p class="text-light pt-3 mt-3 lead">
-                                Save up to <b style="font-size: 26px">20%</b> at the best <br>Healthcare providers near
-                                you!
-                            </p>
-                        </div>
-                        <div class="col-md-4 pt-md-3 mt-md-3">
-                            <i class="fa fa-heartbeat text-light fa-5x float-md-right" aria-hidden="true"></i>
-                        </div>
+
                     </div>
                 </div>
             </div>
@@ -91,7 +80,7 @@
                             <div class="row">
 
                                 <div class="col p-2 my-2 mx-1 text-center">
-                                    <input type="button" onclick="return nominateDependant()" value="Continue" style="background-color: orangered; color: white !important;" class="btn btn-warning pl-4 pr-4 text-light submit-btn"/>
+                                    <input type="button" onclick="return nominateDependant()" value="Continue" style="background-color: orangered;border-color: orangered; color: white !important;" class="btn btn-warning pl-4 pr-4 text-light submit-btn"/>
                                 </div>
                             </div>
                         </div>
--- resources/views/core/member/payment.blade.php
+++ resources/views/core/member/payment.blade.php
@@ -12,21 +12,8 @@
             <div class="container py-md-5">
                 <div class="rounded">
                     <div class="row py-4">
-                        <div class="col-md-8 pt-md-3 mt-md-3">
-                            <h3 class="text-light">
-                                <b>ACCESS AFFORDABLE HEALTHCARE <br>&nbsp;&nbsp;&nbsp;ANYWHERE ANYTIME</b>
-                            </h3>
-                            <p class="text-light pt-3 mt-3 lead">
-                                Save up to <b style="font-size: 26px">20%</b> at the best <br>Healthcare providers near
-                                you!
-                            </p>
-                        </div>
-                        <div class="col-md-4 pt-md-3 mt-md-3">
-                            <i
-                                class="fa fa-heartbeat text-light fa-5x float-md-right"
-                                aria-hidden="true"
-                            ></i>
-                        </div>
+
+
                     </div>
                 </div>
             </div>
@@ -39,9 +26,7 @@
                 class="form_cont container py-md-5 bg-light shadow-lg rounded mb-5"
             >
                 <div class="col-11 mx-auto">
-                    <div class="col-5 col-md-2 mx-auto">
-                        <img src="{{ url('frontend/assets/user.png') }}" alt="User Logo" width="100%" class="user_image mb-2"/>
-                    </div>
+
                     <h5 class="text-center">
                         <caption>
                             Payment Option
@@ -67,7 +52,7 @@
                         <div class="row">
                             <div id="error_alert"></div>
                             <div class="col p-2 my-2 mx-1 text-center">
-                                <input onclick="return requestPayment()" type="button" value="Complete Transaction" style="background-color: orangered; color: white !important;" class="btn btn-warning pl-4 pr-4 text-light"/>
+                                <input onclick="return requestPayment()" type="button" value="Complete Transaction" style="background-color: orangered;border-color: orangered; color: white !important;" class="btn btn-warning pl-4 pr-4 text-light"/>
                             </div>
                         </div>
                     </div>

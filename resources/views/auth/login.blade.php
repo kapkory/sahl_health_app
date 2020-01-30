b10533d844ba41def191e13358f7ee9885912753
levis <leviskapkory@gmail.com>
2020-01-30 16:00:18 +0300

    clear workspace

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -102,24 +102,16 @@
                            <ul class="dt-list dt-list-sm dt-list-cm-0 ml-auto">
                                <li class="dt-list__item">
                                    <!-- Fab Button -->
-                                   <a style="display: none" href="{{ url('facebook/login') }}" class="btn btn-outline-primary dt-fab-btn size-30">
-                                       <i class="icon icon-facebook icon-xl"></i>
-                                   </a>
+{{--                                   <a style="display: none" href="{{ url('facebook/login') }}" class="btn btn-outline-primary dt-fab-btn size-30">--}}
+{{--                                       <i class="icon icon-facebook icon-xl"></i>--}}
+{{--                                   </a>--}}
                                    <!-- /fab button -->
                                </li>
-
-{{--                               <li class="dt-list__item">--}}
-{{--                                   <!-- Fab Button -->--}}
-{{--                                   <a href="{{ url('google/login') }}" class="btn btn-outline-primary dt-fab-btn size-30">--}}
-{{--                                       <i class="icon icon-google-plus icon-xl"></i>--}}
-{{--                                   </a>--}}
-{{--                                   <!-- /fab button -->--}}
-{{--                               </li>--}}
                            </ul>
 
                        </div>
                          <div class="row mt-2">
-                               <a href="{{ url('google/login') }}" class="btn btn-primary btn-block">
+                               <a style="background-color: #335062; border-color: #335062" href="{{ url('google/login') }}" class="btn btn-primary btn-block">
                                    Sign In with Google
                                </a>
 
@@ -144,74 +136,4 @@
 
         </div>
     </div>
-        <!-- /login content -->
-{{--<div class="container">--}}
-{{--    <div class="row justify-content-center">--}}
-{{--        <div class="col-md-8">--}}
-{{--            <div class="card">--}}
-{{--                <div class="card-header">{{ __('Login') }}</div>--}}
-
-{{--                <div class="card-body">--}}
-{{--                    <form method="POST" action="{{ route('login') }}">--}}
-{{--                        @csrf--}}
-
-{{--                        <div class="form-group row">--}}
-{{--                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>--}}
-
-{{--                            <div class="col-md-6">--}}
-{{--                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>--}}
-
-{{--                                @error('email')--}}
-{{--                                    <span class="invalid-feedback" role="alert">--}}
-{{--                                        <strong>{{ $message }}</strong>--}}
-{{--                                    </span>--}}
-{{--                                @enderror--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="form-group row">--}}
-{{--                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>--}}
-
-{{--                            <div class="col-md-6">--}}
-{{--                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">--}}
-
-{{--                                @error('password')--}}
-{{--                                    <span class="invalid-feedback" role="alert">--}}
-{{--                                        <strong>{{ $message }}</strong>--}}
-{{--                                    </span>--}}
-{{--                                @enderror--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="form-group row">--}}
-{{--                            <div class="col-md-6 offset-md-4">--}}
-{{--                                <div class="form-check">--}}
-{{--                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>--}}
-
-{{--                                    <label class="form-check-label" for="remember">--}}
-{{--                                        {{ __('Remember Me') }}--}}
-{{--                                    </label>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-
-{{--                        <div class="form-group row mb-0">--}}
-{{--                            <div class="col-md-8 offset-md-4">--}}
-{{--                                <button type="submit" class="btn btn-primary">--}}
-{{--                                    {{ __('Login') }}--}}
-{{--                                </button>--}}
-
-{{--                                @if (Route::has('password.request'))--}}
-{{--                                    <a class="btn btn-link" href="{{ route('password.request') }}">--}}
-{{--                                        {{ __('Forgot Your Password?') }}--}}
-{{--                                    </a>--}}
-{{--                                @endif--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </form>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
 @endsection

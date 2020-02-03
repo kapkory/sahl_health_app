8b40b74e3fdaac09f4ce4f246d77a57de8aa1967
levis <leviskapkory@gmail.com>
2020-02-03 10:50:04 +0300

    updated agent profile page

--- resources/views/core/provider/index.blade.php
+++ resources/views/core/provider/index.blade.php
@@ -11,14 +11,14 @@
     <div class="col-xl-12">
         <div class="row">
             <div class="col-md-3 col-6"><!-- Card -->
-                <div class="dt-card"><!-- Card Body -->
-                    <div class="dt-card__body p-xl-8 py-sm-8 py-6 px-4">
-                        <span class="badge badge-secondary badge-top-right">Revenue</span><!-- Media -->
+                <div class="dt-card" style="background-color: #7BB37D;border-radius: 10px;"><!-- Card Body -->
+                    <div  class="dt-card__body p-xl-8 py-sm-8 py-6 px-4" >
+
                         <div class="media">
-                            <i class="icon icon-revenue-new icon-5x mr-xl-5 mr-3 align-self-center"></i><!-- Media Body -->
+                            <i class="icon icon-revenue-new text-white icon-5x mr-xl-5 mr-3 align-self-center"></i><!-- Media Body -->
                             <div class="media-body">
-                                <p class="mb-1 h1">{{ @number_format($data['revenue'],2) }}</p>
-                                <span class="d-block text-light-gray">Ksh</span>
+                                <p class="mb-1 h1 text-white">Ksh <br>{{ @number_format($data['revenue'],2) }}</p>
+                                <span class="d-block text-black-50">Revenue</span>
                             </div><!-- /media body -->
                         </div><!-- /media -->
                     </div><!-- /card body -->
@@ -26,46 +26,48 @@
             </div>
 
             <div class="col-md-3 col-6"><!-- Card -->
-                <div class="dt-card"><!-- Card Body -->
-                    <div class="dt-card__body p-xl-8 py-sm-8 py-6 px-4">
-                        <span class="badge badge-info badge-top-right">Customer Savings</span><!-- Media -->
+                <div class="dt-card" style="background: rgba(196, 196, 196, 0.42);border-radius: 10px;"><!-- Card Body -->
+                    <div  class="dt-card__body p-xl-8 py-sm-8 py-6 px-4" >
+
                         <div class="media">
                             <i class="icon icon-orders-new icon-5x mr-xl-5 mr-3 align-self-center"></i><!-- Media Body -->
                             <div class="media-body">
-                                <p class="mb-1 h1">{{ @number_format($data['customer_savings'],2) }}</p>
-                                <span class="d-block text-light-gray">Ksh</span>
+                                <p class="mb-1 h1">Ksh <br>{{ @number_format($data['customer_savings'],2) }}</p>
+                                <span class="d-block text-black-50">Savings</span>
                             </div><!-- /media body -->
                         </div><!-- /media -->
                     </div><!-- /card body -->
                 </div><!-- /card -->
             </div>
 
-            <a href="{{ url('provider/visits') }}" class="col-md-3">
-                <div class="dt-card">
+            <a href="{{ url('provider/visits') }}" class="col-md-3 col-6"><!-- Card icon-contacts-app-->
+                <div class="dt-card" style="background: #335062; border-color:#F07A3B; border-radius: 10px;box-sizing: border-box;"><!-- Card Body -->
+                    <div  class="dt-card__body p-xl-8 py-sm-8 py-6 px-4 py-1" >
 
-                    <!-- Card Body -->
-                    <div class="dt-card__body px-5 py-4">
-                        <span class="badge badge-success badge-top-right">Hospital Visits</span>                        <div class="d-flex align-items-baseline mb-4">
-                            <span class="display-4 text-center font-weight-500 text-dark ">{{ $data['visits'] }} visit(s)</span>
-                        </div>
-
-                        <div class="dt-indicator-item__info mb-2" data-fill="100" data-max="100">
-                            <div class="dt-indicator-item__bar">
-                                <div class="dt-indicator-item__fill fill-pointer bg-primary" style="width: 100%;"></div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <!-- /bard body -->
-
-                </div>
+                        <div class="media">
+                            <i class="icon text-white icon-contacts icon-5x mr-xl-5 mr-3 align-self-center"></i><!-- Media Body -->
+                            <div class="media-body">
+                                <p class="mb-1 h1 text-center text-white" style="color: #335062">{{ @$data['visits'] }}</p>
+                                <span class="d-block text-center text-white" style="color: #335062">&nbsp;&nbsp;Hospital <br>&nbsp;Visits</span>
+                            </div><!-- /media body -->
+                        </div><!-- /media -->
+                    </div><!-- /card body -->
+                </div><!-- /card -->
             </a>
 
             @if(auth()->user()->referral_code)
                 <div class="col-md-3 col-6"><!-- Card -->
-                    <div class="dt-card"><!-- Card Body -->
-                        <div class="dt-card__body p-xl-8 py-sm-8 py-6 px-4">
-                         <button href="#refer_member"  data-toggle="modal"  class="btn btn-outline-info">Refer Member</button>
+                    <div class="dt-card" style="background-color: #F07A3B;border-radius: 10px;"><!-- Card Body -->
+                        <div  class="dt-card__body p-xl-8 py-sm-8 py-6 px-4" >
+
+                            <div class="media">
+                                <i class="icon icon-crm text-white icon-5x mr-xl-5 mr-3 align-self-center"></i><!-- Media Body -->
+                                <div class="media-body">
+                                    <br>
+                                    <button href="#refer_member"  data-toggle="modal"  class="btn text-white" style="border-color: white">Refer Member</button>
+                                </div><!-- /media body -->
+
+                            </div><!-- /media -->
                         </div><!-- /card body -->
                     </div><!-- /card -->
                 </div>

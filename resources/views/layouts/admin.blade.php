64cf7d06e6ff8c92f283c54728bc0b4a54535abc
levis <leviskapkory@gmail.com>
2020-02-03 10:52:29 +0300

    updated agent profile page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -227,7 +227,7 @@
                                 @if(auth()->user()->role == 'provider')
                                     <a href="#" class="dt-nav__link dropdown-toggle no-arrow dt-avatar-wrapper"
                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <img class="dt-avatar size-30" src="{{ url('drift') }}/assets/images/user-avatar/domnic-harris.jpg" alt="Domnic Harris">
+                                        <img class="dt-avatar size-30" src="{{ url('frontend/assets/user.png') }}" alt="Provider Avatar">
                                         <span class="dt-avatar-info d-none d-sm-block">
                                             @if(auth()->user()->institution_id)
                                                 <span class="dt-avatar-name">{{ auth()->user()->getInstitutionName() }}</span>

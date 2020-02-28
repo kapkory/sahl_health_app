cd597a5345b47d4ac40882737701c8046b56be7d
levis <leviskapkory@gmail.com>
2020-02-28 12:37:24 +0300

    recent updates

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -38,7 +38,7 @@
                             <label for="phone">{{ __('Email Address') }}</label>
 
 
-                            <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                             @error('phone')
                             <span class="invalid-feedback" role="alert">

51a4bb0392a38af6b637ff45bdaab1dcf0f16c7c
levis <leviskapkory@gmail.com>
2020-02-28 16:37:10 +0300

    clear workspace

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@sahlhealth.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'support@sahlhealth.com'),
         'name' => env('MAIL_FROM_NAME', 'Sahlhealth'),
     ],
 
